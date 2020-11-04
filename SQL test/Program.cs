b0110eac0f74079ff019d2c2d5ce60fbdd3d7982
Mattias <m.heijboer2@students.uu.nl>
2020-11-04 22:21:45 +0100

    SQL test

---
+++ SQL test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQL_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection("Server=localhost;" + "Database=Database1;");
+            conn.Open();
+            SqlCommand command = conn.CreateCommand();
+            command.CommandText = "Select * from Names";
+        }
+    }
+}
