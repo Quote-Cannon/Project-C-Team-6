1cc8e6f4f2e2275f8c6764e5c935b1b756dc0500
keremz <keremz@users.noreply.github.com>
2020-11-05 12:57:30 +0100

    Verbinding gemaakt met pgAdmin

---
+++ PostgreSql_and_Csharp-master/PostgreSql_and_Csharp-master/PostgreSQL/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace PgSql
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbDataItems = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(262, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(357, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get data from product table";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbDataItems
+            // 
+            this.tbDataItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDataItems.Location = new System.Drawing.Point(1, 107);
+            this.tbDataItems.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbDataItems.Multiline = true;
+            this.tbDataItems.Name = "tbDataItems";
+            this.tbDataItems.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbDataItems.Size = new System.Drawing.Size(885, 409);
+            this.tbDataItems.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 532);
+            this.Controls.Add(this.tbDataItems);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button button1;
+      private System.Windows.Forms.TextBox tbDataItems;
+   }
+}
+
