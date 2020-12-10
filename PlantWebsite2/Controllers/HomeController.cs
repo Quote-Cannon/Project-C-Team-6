e34af57eaef3e0579f9188a407af974843196653
jaspereck <60735321+jaspereck@users.noreply.github.com>
2020-12-10 21:36:20 +0100

    chatpage

--- PlantWebsite2/Controllers/HomeController.cs
+++ PlantWebsite2/Controllers/HomeController.cs
@@ -34,6 +34,12 @@ namespace PlantWebsite.Controllers
             return View();
         }
 
+        public IActionResult Chat()
+        {
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
