f4e4322604b7e4ba530edc9c20ddb91d54a21fad
Mattias <m.heijboer2@students.uu.nl>
2020-12-18 00:30:26 +0100

    afbeeldingen kunnen geedit worden

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -129,17 +129,32 @@ namespace AuthSystem.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,LatinName,Description,Picture,Kind,Type,Water,Light,ProductDate,Trade")] Product product)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,LatinName,Description,Kind,Type,Water,Light,ProductDate,Trade")] Product product, IFormFile Picture)
         {
             if (id != product.Id)
             {
                 return NotFound();
             }
-
-            if (ModelState.IsValid)
+            byte[] streamOutput;
+            string output = "";
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Picture.CopyTo(ms);
+                streamOutput = ms.ToArray();
+            }
+            foreach (byte b in streamOutput)
+            {
+                string number = Convert.ToString(Convert.ToInt32(b));
+                while (number.Length < 3)
+                    number = "0" + number;
+                output += number;
+            }
+            //This clause is supposed to check if the only error is an empty Picture field, since I (Mattias) can't find a way to get rid of it. Any other error should still trigger the clause.
+            if (ModelState["Picture"].RawValue == null && ModelState.ErrorCount == 1)
             {
                 try
                 {
+                    product.Picture = output;
                     _context.Update(product);
                     await _context.SaveChangesAsync();
                 }
