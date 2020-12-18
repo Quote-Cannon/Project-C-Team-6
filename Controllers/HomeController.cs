51c9a4080a2755a1195cf34ffe6f527152f4f933
keremz <keremz@users.noreply.github.com>
2020-12-18 10:51:02 +0100

    aboutus

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,6 +40,13 @@ namespace AuthSystem.Controllers
             return View();
         }
 
+        public IActionResult Aboutus()
+        {
+
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
