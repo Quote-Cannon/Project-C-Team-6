e239a871d0d328bc3677b211d9017f477e017a10
jaspereck <60735321+jaspereck@users.noreply.github.com>
2021-01-04 14:33:10 +0100

    email link

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,10 +4,6 @@
 // Write your JavaScript code.
 function objectFit(image) {
     if ('objectFit' in document.documentElement.style === false && image.currentStyle['object-fit']) {
-        image.style.background = 'url("' + image.src + '") no-repeat 50%/' + image.currentStyle['object-fit'];
-        image.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='" + image.width + "' height='" + image.height + "'%3E%3C/svg%3E";
+        image.style ="width='100%' height='100%'";
     }
 }
-
-var elem = document.getElementById('cover-image')
-objectFit(elem);
