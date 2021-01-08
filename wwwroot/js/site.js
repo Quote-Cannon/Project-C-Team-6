8fe491d9ef476e708db451870b25e28ecc9019db
jaspereck <60735321+jaspereck@users.noreply.github.com>
2021-01-08 11:24:03 +0100

    chat

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,8 +2,4 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function objectFit(image) {
-    if ('objectFit' in document.documentElement.style === false && image.currentStyle['object-fit']) {
-        image.style ="width='100%' height='100%'";
-    }
-}
+
