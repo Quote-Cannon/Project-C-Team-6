148d600f88c099d7d759e9dcc6db8fd041420cd1
0980991 <0980991@users.noreply.github.com>
2021-01-15 11:15:41 +0100

    FONCS

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using AuthSystem.Data;
 using AuthSystem.Models;
 using Microsoft.Extensions.Localization;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Localization;
 
 namespace AuthSystem.Controllers
 {
@@ -18,6 +21,8 @@ namespace AuthSystem.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly AuthDbContext _context;
         private readonly IStringLocalizer _localizer;
+
+        public string currentAction = "";
 
         public HomeController(ILogger<HomeController> logger, AuthDbContext context, IStringLocalizer<HomeController> localizer)
         {
@@ -50,6 +55,18 @@ namespace AuthSystem.Controllers
             return View();
         }
 
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CultureManagement(string culture)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new Microsoft.AspNetCore.Http.CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
+            return RedirectToAction(nameof(Index));
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
